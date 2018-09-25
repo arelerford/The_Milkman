4d5a33b51ce283136e4beca13da4d2d0f357752e
Student <student@lab-240-013.cs.csubak.edu>
2018-09-25 12:03:39 -0700

    changes to credits file

--- alexH.cpp
+++ alexH.cpp
@@ -1,17 +1,19 @@
 //Alex Hamilton
+//Milkman Credits
 #include <iostream>
 #include "fonts.h"
+#include <GL/glx.h>
 using namespace std;
 
-extern void show_credits();
-extern void render(void);
+//extern void show_credits();
+//extern void render(void);
 //extern checkKeys(XEvent *e);
 
 
-void show_credits(int x, int y);
+void show_credits(Rect r, int x, int y);
 {
-    	r.bot = gl.yres - 20;
-    	r.left = 10;
-	r.center = 0;
+    //	r.bot = gl.yres - 20;
+    //	r.left = 10;
+//	r.center = 0;
 	ggprint8b(&r, 16, c, "Alex Hamilton"); 
 }
