7893f3db900ad18bfed585739465ce04519e7927
Student <student@lab-240-035.cs.csubak.edu>
2018-10-02 11:24:33 -0700

    Justin S. Show Image Function

--- justinS.cpp
+++ justinS.cpp
@@ -11,3 +11,25 @@ void show_credits_justin(Rect r, int x, int y)
 
     ggprint8b(&r, x, y, "Justin S.");
 }
+
+void show_justin_image (int x, int y, GLuint textid)
+{
+    glColor3f (1.0f, 1.0f, 1.0f);
+    
+    int wid = 40;
+
+    glPushMatrix();
+
+    glTranslatef(x, y, 0);
+    
+    glBindTexture(GL_TEXTURE_2D, textid);
+
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f); glVertex2i (-wid, -wid);
+        glTexCoord2f(0.0f, 0.0f); glVertex2i (-wid, wid);
+        glTexCoord2f(1.0f, 0.0f); glVertex2i (wid, wid);
+        glTexCoord2f(1.0f, 1.0f); glVertex2i (-wid, wid);
+    glEnd();
+
+    glPopMatrix();
+}
