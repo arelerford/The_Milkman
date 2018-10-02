0daa4aaa0100ddb61d20fd56b5f1721c1b295be9
Isaac Lara <ilara@odin.cs.csubak.edu>
2018-10-01 19:41:02 -0700

    Fix isaac.cpp postion

--- isaacL.cpp
+++ isaacL.cpp
@@ -1,7 +1,12 @@
 #include "fonts.h"
 #include <GL/glx.h>
 
-void show_isaac(Rect r, int x, int y) {
+void show_isaac(Rect r, int x, int y) 
+{
+    r.bot = 600 - 40;
+	r.left = 10;
+	r.center = 0;
+	
 	ggprint8b(&r, x, y, "Isaac Lara");
 }
 
