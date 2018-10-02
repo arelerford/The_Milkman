1301f418a97ddef948bc6aea6edf0c4b006da56f
Isaac <ilara2@csub.edu>
2018-10-02 12:58:15 -0700

    Added pic

--- isaacL.cpp
+++ isaacL.cpp
@@ -10,3 +10,24 @@ void show_isaac(Rect r, int x, int y)
 	ggprint8b(&r, x, y, "Isaac Lara");
 }
 
+void showIsaacPic(int x, int y, GLuint textid)
+{
+    glColor3f (1.0f, 1.0f, 1.0f);
+    
+    int wid = 40;
+
+    glPushMatrix();
+
+    glTranslatef(x, y, 0);
+    
+    glBindTexture(GL_TEXTURE_2D, textid);
+
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f); glVertex2i (-wid, -wid);
+        glTexCoord2f(0.0f, 0.0f); glVertex2i (-wid, wid);
+        glTexCoord2f(1.0f, 0.0f); glVertex2i (wid, wid);
+        glTexCoord2f(1.0f, 1.0f); glVertex2i (wid, -wid);
+    glEnd();
+
+    glPopMatrix();
+}
