f691cec596bc020941f5fb7c4a2922fea3b59872
Austin Relerford <arelerford@odin.cs.csubak.edu>
2018-12-02 20:55:09 -0800

    gl nonsense

--- austinR.h
+++ austinR.h
@@ -10,10 +10,23 @@ class Bullet {
 public:
     Vec pos;
     Vec vel;
+    GLuint texid;
+    Image bottle_img = "./images/player/bottle.png";
     float color[3];
     struct timespec time;
 public:
-    Bullet() { }
+    Bullet() {
+    glGenTextures(1, &texid);
+        glBindTexture(GL_TEXTURE_2D, texid);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        unsigned char *new_img = buildAlphaData(&bottle_img);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, bottle_img.width, bottle_img.height, 0,GL_RGBA, GL_UNSIGNED_BYTE, new_img);
+        free(new_img);
+    printf("%u\n",texid );
+    
+    }   
+    }
 };
 
 class Game {
